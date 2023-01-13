d09a628b980d0aaf6a10680775c910b9fa9f0175
Nabil <nallis014@gmail.com>
2023-01-13 19:25:18 -0300

    prueba4

--- client/src/components/Login/UserGoogle.jsx
+++ client/src/components/Login/UserGoogle.jsx
@@ -1,28 +1,28 @@
-import axios from "axios";
-import jwt_decode from "jwt-decode";
+// import axios from "axios";
+// import jwt_decode from "jwt-decode";
 
-export const createOrGetUserGoogle = async (user) => {
-  const decoded = jwt_decode(user.credential)
+// export const createOrGetUserGoogle = async (user) => {
+//   const decoded = jwt_decode(user.credential)
 
-  const { email, name, sub, family_name, given_name } = decoded;
+//   const { email, name, sub, family_name, given_name } = decoded;
 
-  const userObject = {
-    _id: sub,
-    name: name,
-    email,
-    lastname: family_name,
-    name: given_name,
-  }
-
-    //forma antigua
-  //  localStorage.setItem("userInfo", JSON.stringify(userObject));
-  //  await axios.post("/api/login/google", userObject);
-
-  const infoGuest = await axios.post("/user", userObject);
-//   if(infoGuest.data === "Usuario Baneado"){
-//    throw new Error
+//   const userObject = {
+//     _id: sub,
+//     name: name,
+//     email,
+//     lastname: family_name,
+//     name: given_name,
 //   }
-  console.log("aqui",infoGuest.data)
-  localStorage.setItem("userInfo", JSON.stringify(infoGuest.data));
-  return infoGuest  
-};
+
+//     //forma antigua
+//   //  localStorage.setItem("userInfo", JSON.stringify(userObject));
+//   //  await axios.post("/api/login/google", userObject);
+
+//   const infoGuest = await axios.post("/user", userObject);
+// //   if(infoGuest.data === "Usuario Baneado"){
+// //    throw new Error
+// //   }
+//   console.log("aqui",infoGuest.data)
+//   localStorage.setItem("userInfo", JSON.stringify(infoGuest.data));
+//   return infoGuest  
+// };
